3eb3b510cb5a50c75e7c62cd731450d78c5f0cee
hithesh0160 <Hithesh Puthran@Hithesh-HPLaptop>
2022-12-28 00:12:31 +0530

    Cool

--- New/src/com/naukri/testscript/Naukri.java
+++ New/src/com/naukri/testscript/Naukri.java
@@ -7,6 +7,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Naukri {
 	static {
@@ -17,16 +19,18 @@ public class Naukri {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://www.naukri.com");
+		WebDriverWait wait=new WebDriverWait(driver, 20);
 		driver.manage().window().maximize();
 		driver.findElement(By.linkText("Login")).click();
 		driver.findElement(By.xpath("//label[text()='Email ID / Username']/../input")).sendKeys("hitesh_p16@yahoo.com");
 		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("abc1234@");
-		driver.findElement(By.xpath("//button[text()='Login']")).click();		
-		driver.findElement(By.xpath("//div[text()='UPDATE PROFILE']")).click();
-		File f=new File("./data/Mr. Hithesh_Resume.pdf");
+		driver.findElement(By.xpath("//button[text()='Login']")).click();	
+		wait.until(ExpectedConditions.titleIs("Home | Mynaukri"));
+		driver.get("https://www.naukri.com/mnjuser/profile?id=&altresid");
+//		driver.findElement(By.xpath("//div[text()='UPDATE PROFILE']")).click();
+		File f=new File("./data/Mr.Hithesh_Experienced_Tester_Resume.pdf");
 		String absolutePath = f.getAbsolutePath();
 		driver.findElement(By.id("attachCV")).sendKeys(absolutePath);
-		Thread.sleep(3000);
-		driver.close();
+//		driver.close();
 	}
 }
