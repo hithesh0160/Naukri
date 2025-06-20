def93e51a4437534d32c3fb0c3adeb80c6037aa9
hithesh0160 <hitheshputhran0160@gmail.com>
2025-06-21 00:02:50 +0530

    smq cwjd

--- New/src/com/naukri/testscript/Naukri.java
+++ New/src/com/naukri/testscript/Naukri.java
@@ -37,7 +37,7 @@ public class Naukri {
 		// options.setProxy(proxy);
 		options.addArguments("--window-size=1920,1080");
 		options.addArguments("--no-sandbox");
-		// options.addArguments("--headless=new");
+		options.addArguments("--headless=new");
 		options.addArguments("--disable-dev-shm-usage");
 		options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
 
